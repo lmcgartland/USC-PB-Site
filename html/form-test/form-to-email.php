3d9deb8bc0d53acf45786e4c3c0cbcd7f40105e3
lmcgartland <luke.mcgartland@gmail.com>
2016-02-24 12:32:57 -0800

    Receipt

--- html/form-test/form-to-email.php
+++ html/form-test/form-to-email.php
@@ -120,7 +120,7 @@ $attachment
 //Send the email!
 mail($to,$email_subject,$message,$headers);
 
-$receipt = "Here is your receipt for your graphics request. Please send any follow up emails to pbgraphics@usc.edu and include your case number in the subject.\n\n$email_body"
+$receipt = "Here is your receipt for your graphics request. Please send any follow up emails to pbgraphics@usc.edu and include your case number in the subject.\n\n$email_body";
 $receipt_headers = "From: pbgraphics@usc.edu \r\n";
 $receipt_headers .= "Reply-To: pbgraphics@usc.edu";
 mail($user_email,$email_subject,$receipt,$receipt_headers);
