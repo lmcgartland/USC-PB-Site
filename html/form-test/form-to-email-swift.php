696e69e55a42822c1624480294ad1243f8de29f8
lmcgartland <luke.mcgartland@gmail.com>
2016-03-08 14:47:42 -0800

    2 factor auth

--- html/form-test/form-to-email-swift.php
+++ html/form-test/form-to-email-swift.php
@@ -196,7 +196,7 @@ debug_to_console( "created message" );
 
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 25, 'tls');
 $transport->setUsername('temp.web.pb@gmail.com');
-$transport->setPassword('vkc-xGm-3dQ-6mT');
+$transport->setPassword('dcncobfxmhlksoao');
 
 debug_to_console( "created transport" );
 
