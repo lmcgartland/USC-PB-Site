5dfe6a054132be85b5eae4757be220c5e5e1687b
lmcgartland <luke.mcgartland@gmail.com>
2016-03-10 14:36:54 -0800

    Got rid of if

--- html/form-test/form-to-email-swift.php
+++ html/form-test/form-to-email-swift.php
@@ -145,14 +145,14 @@ $message = Swift_Message::newInstance()
   //->addPart('<q>Here is the message itself</q>', 'text/html')
  ;
 debug_to_console( "About to Null Check" );
-if !is_null($_FILES['file']['tmp_name']){
+/*if !is_null($_FILES['file']['tmp_name']){
   debug_to_console( "Attachment there" );
   $message->attach(
     Swift_Attachment::fromPath($_FILES['file']['tmp_name'])->setFilename($_FILES['file']['name'])
   );
 }else{
   debug_to_console("Attachment not there");
-}
+}*/
 debug_to_console( "Null Check over" );
 
 
